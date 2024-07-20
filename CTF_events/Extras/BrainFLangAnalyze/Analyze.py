a7c35ddd1afaae7c668f8ce9c18fa55d43a75f48
Pak Cyberbot <93365275+PakCyberbot@users.noreply.github.com>
2024-07-20 19:45:42 +0500

    Add files via upload

---
+++ CTF_events/Extras/BrainFLangAnalyze/Analyze.py
@@ -0,0 +1,25 @@
+# Imaginary CTF 2024 BF challenge of forensics
+
+import re
+
+code=open("bfcode.txt","r").read()
+
+# loops minus
+loop_matches1 = re.findall(r'>>(\+*)\[', code)
+
+# [<+++>-]
+loop_matches2 = re.findall(r'\[<(\+*)>-\]', code)
+
+numbers_to_minus = []
+for val1, val2 in zip(loop_matches1, loop_matches2):
+    numbers_to_minus.append(len(val1)*len(val2))
+
+# print(numbers_to_minus)
+
+# Main val
+# <[-<+>]<-----------[><]
+
+main_matches = re.findall(r'<\[-<\+>\]<(\-*)\[><\]', code)
+
+for main_val, number in zip(main_matches,numbers_to_minus):
+    print(chr(len(main_val)-number),end='')
